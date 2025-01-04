17824cf1643d126c9b367ce92ef6ce26ed35858d
Cyprien Ménard <cyprien.menard@reseau.eseo.fr>
2025-01-04 20:25:50 +0100

    move time tools from older project

---
+++ src/tools/timeTools/timeMs.cpp
@@ -0,0 +1,93 @@
+#include "timeMs.h"
+
+#include "api/time/time.h"
+
+timeMs::timeMs()
+{
+    m_timeMs = 0;
+}
+
+timeMs::timeMs(time_ms_t timeMs)
+{
+    m_timeMs = timeMs;
+}
+
+void timeMs::set(time_ms_t timeMs)
+{
+    m_timeMs = timeMs;
+}
+
+void timeMs::setNow()
+{
+    m_timeMs = nowMs();
+}
+
+time_ms_t timeMs::nowMs()
+{
+    return time_ms();
+}
+
+time_ms_t timeMs::get()
+{
+    return m_timeMs;
+}
+
+time_ms_t timeMs::toSeconds()
+{
+    return m_timeMs / 1000;
+}
+
+time_ms_t timeMs::toMinutes()
+{
+    return m_timeMs / 60000;
+}
+
+timeMs timeMs::operator+(const timeMs &time)
+{
+    return timeMs(m_timeMs + time.m_timeMs);
+}
+
+timeMs timeMs::operator-(const timeMs &time)
+{
+    return timeMs(m_timeMs - time.m_timeMs);
+}
+
+timeMs timeMs::operator*(const timeMs &time)
+{
+    return timeMs(m_timeMs * time.m_timeMs);
+}
+
+timeMs timeMs::operator/(const timeMs &time)
+{
+    return timeMs(m_timeMs / time.m_timeMs);
+}
+
+bool timeMs::operator==(const timeMs &time)
+{
+    return m_timeMs == time.m_timeMs;
+}
+
+bool timeMs::operator!=(const timeMs &time)
+{
+    return m_timeMs != time.m_timeMs;
+}
+
+bool timeMs::operator>(const timeMs &time)
+{
+    return m_timeMs > time.m_timeMs;
+}
+
+bool timeMs::operator<(const timeMs &time)
+{
+    return m_timeMs < time.m_timeMs;
+}
+
+bool timeMs::operator>=(const timeMs &time)
+{
+    return m_timeMs >= time.m_timeMs;
+}
+
+bool timeMs::operator<=(const timeMs &time)
+{
+    return m_timeMs <= time.m_timeMs;
+}
