d5e07dbaf038b00f3cf5692690471000c122ebde
Cyprien Ménard <cyprien.menard@reseau.eseo.fr>
2025-07-24 19:00:10 +0200

    Remove Master class implementation and associated header files to streamline codebase

--- cmake/targets/master/master.cmake
+++ cmake/targets/master/master.cmake
@@ -10,7 +10,6 @@ add_compile_options(-D__TARGET_MASTER)
 add_sources(
     master/masterLaunch.cpp
     master/session/session.cpp
-    master/master.cpp
 
     animation/animation.cpp
     animation/animationManager.cpp
